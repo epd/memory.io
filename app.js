36fdbf4f958aeb6466e4a8ea94ab440711321bd2
Evan Dudla <me@evandudla.com>
2012-01-03 16:18:04 -0500

    Refactored application JS and added delete functionality

--- app.js
+++ app.js
@@ -1,70 +1,105 @@
-$(function() {
+(function($) {
   /**
    * Bucket Lists
    */
-   // Model
-   Bucket = Backbone.Model.extend();
-   // Collection
-   window.BucketList = Backbone.Collection.extend({
-     model: Bucket,
-     localStorage: new Store("buckets"),
-   });
-   window.Buckets = new BucketList;
-   // View
-   window.BucketView = Backbone.View.extend({
-     tagName: "li",
-     render: function() {
-       var title = this.model.get('title') + "<b>&rarr;</b>";
-       var link = '<a href="javascript:void(0);">' + title + '</a>';
-       $(this.el).html(link);
-       return this;
-     },
-   });
+  // Model
+  Bucket = Backbone.Model.extend();
+
+  // Collection
+  window.BucketList = Backbone.Collection.extend({
+    model: Bucket,
+    localStorage: new Store("buckets"),
+  });
+  window.Buckets = new BucketList;
+
+  // View
+  window.BucketView = Backbone.View.extend({
+    tagName: "li",
+    render: function() {
+      $(this.el)
+        .attr("data-id", this.model.get('id'))
+        .html('<a href="javascript:void(0);">' + this.model.get('title') + '</a>');
+      return this;
+    },
+  });
 
   AppView = Backbone.View.extend({
-    el: $("#buckets"),
+    el: $("#app"),
 
     /**
      * Initialization
      */
-     initialize: function() {
-       _.bindAll(this, 'addSingleBucket', 'addExistingBuckets');
-       this.input = this.$("#create-bucket");
-       Buckets.bind("add", this.addSingleBucket);
-       Buckets.bind("reset", this.addExistingBuckets);
-       Buckets.fetch();
-     },
+    initialize: function() {
+      _.bindAll(this, 'render', 'addBucket');
+      Buckets.bind("add", this.addBucket);
+      Buckets.bind("destroy", this.render);
+      Buckets.fetch();
+      this.render();
+    },
+
+    // View setup
+    render: function () {
+      // Bucket list wrapper
+      if (!this.wrapper) {
+        this.wrapper = $('<section/>', {id: 'bucket-lists'}).appendTo(this.el);
+
+        $('<h1/>', {text: 'My Bucket List'})
+          .appendTo('<header/>')
+          .appendTo(this.wrapper);
+
+        this.input = $('<input/>', {
+          placeholder: 'What do you want to do...',
+          id: 'create-bucket'
+        }).appendTo(this.wrapper);
+
+        this.list = $('<ul/>').appendTo(this.wrapper);
+      }
+
+      // Populate our list with existing Buckets
+      this.list.html('');
+      Buckets.each(this.addBucket);
+    },
 
     /**
      * Event listeners
      */
-     events: {
-       "keypress #create-bucket": "newBucketOnEnter",
-     },
+    events: {
+      "keypress #create-bucket": "createBucket",
+      "click #bucket-lists a": "removeBucket",
+    },
 
     /**
      * Event callbacks
      */
-     // Create a new Bucket on submit of input field
-     newBucketOnEnter: function(e) {
-       if (e.keyCode !== 13) {
-         return;
-       }
-       var i = $("#create-bucket");
-       if (i && this.input.val()) {
-         Buckets.create({title: this.input.val()})
-       }
-       i.val('');
-     },
-     // Add a single Bucket to our Store
-     addSingleBucket: function(bucket) {
-       var view = new BucketView({model: bucket});
-       this.$("#bucket-lists").append(view.render().el);
-     },
-     // Add our existing Buckets from our Store
-     addExistingBuckets: function() {
-       Buckets.each(this.addSingleBucket);
-     },
+    // Create a new Bucket on submit of input field
+    createBucket: function(e) {
+      // Only continue if Enter key is pressed
+      if (e.keyCode !== 13) {
+        return;
+      }
+
+      // Create our new list if not blank
+      var i = $("#create-bucket");
+      if (i && this.input.val()) {
+        Buckets.create({
+          title: this.input.val()
+        })
+      }
+      i.val('');
+    },
+
+    // Add a single Bucket to our Store
+    addBucket: function(bucket) {
+      var view = new BucketView({model: bucket});
+      this.$("#bucket-lists ul").append(view.render().el);
+    },
+
+    // Remove a single Bucket from the Store
+    removeBucket: function(e) {
+      var id = $(e.target).parents("li").data("id")
+        , current = Buckets.get(id);
+      current.destroy();
+    },
   });
   App = new AppView;
-});
+})(jQuery);
